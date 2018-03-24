0f9045e6956abbe912380973e121a45ccf0ffd48
ihook98 <ihook98@hotmail.com>
2018-03-24 18:17:23 +0900

    Modify main parameters for Unifycoin

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -50,8 +50,8 @@ static CBlock CreateGenesisBlock(const char* pszTimestamp, const CScript& genesi
  */
 static CBlock CreateGenesisBlock(uint32_t nTime, uint32_t nNonce, uint32_t nBits, int32_t nVersion, const CAmount& genesisReward)
 {
-    const char* pszTimestamp = "NY Times 05/Oct/2011 Steve Jobs, Apple’s Visionary, Dies at 56";
-    const CScript genesisOutputScript = CScript() << ParseHex("040184710fa689ad5023690c80f3a49c8f13f8d45b8c857fbcbc8bc4a8e4d3eb4b10f4d4604fa08dce601aaf0f470216fe1b51850b4acf21b179c45070ac7b03a9") << OP_CHECKSIG;
+    const char* pszTimestamp = "Residents safety warnings fell on deaf ears";
+    const CScript genesisOutputScript = CScript() << ParseHex("04a0b0c1f1bcb431b6d8e886885d454fc252a404c6af42cb48410d319bf9445ba12bffaa625889204a243dd25a8cdd4e4f8db058f07332aa91187cc012182549cc") << OP_CHECKSIG;
     return CreateGenesisBlock(pszTimestamp, genesisOutputScript, nTime, nNonce, nBits, nVersion, genesisReward);
 }
 
@@ -97,39 +97,39 @@ public:
         consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nTimeout = 1517356801; // January 31st, 2018
 
         // The best chain should have at least this much work.
-        consensus.nMinimumChainWork = uint256S("0x000000000000000000000000000000000000000000000006805c7318ce2736c0");
+        consensus.nMinimumChainWork = uint256S("0x0000000000000000000000000000000000000000000000000000000000000000");
 
         // By default assume that the signatures in ancestors of this block are valid.
-        consensus.defaultAssumeValid = uint256S("0x1673fa904a93848eca83d5ca82c7af974511a7e640e22edc2976420744f2e56a"); //1155631
+        consensus.defaultAssumeValid = uint256S("0x4f1954e92baf4e94e1e45c3dc6f9fb248da120c33d6f750560a6db116de8eabd"); //194333
 
         /**
          * The message start string is designed to be unlikely to occur in normal data.
          * The characters are rarely used upper ASCII, not valid as UTF-8, and produce
          * a large 32-bit integer with any alignment.
          */
-        pchMessageStart[0] = 0xfb;
-        pchMessageStart[1] = 0xc0;
-        pchMessageStart[2] = 0xb6;
-        pchMessageStart[3] = 0xdb;
-        nDefaultPort = 9333;
+        pchMessageStart[0] = 0xc4;
+        pchMessageStart[1] = 0x47;
+        pchMessageStart[2] = 0xf9;
+        pchMessageStart[3] = 0xee;
+        nDefaultPort = 18649;
         nPruneAfterHeight = 100000;
 
-        genesis = CreateGenesisBlock(1317972665, 2084524493, 0x1e0ffff0, 1, 50 * COIN);
+        genesis = CreateGenesisBlock(1497449710, 848210, 0x1e0ffff0, 1, 3000 * COIN);
         consensus.hashGenesisBlock = genesis.GetHash();
-        assert(consensus.hashGenesisBlock == uint256S("0x12a765e31ffd4059bada1e25190f6e98c99d9714d334efa41a195a7e7e04bfe2"));
-        assert(genesis.hashMerkleRoot == uint256S("0x97ddfbbae6be97fd6cdf3e7ca13232a3afff2353e29badfab7f73011edd4ced9"));
+        assert(consensus.hashGenesisBlock == uint256S("0x943dc625657143761f0f01dbf08c45a02260fc4089a27b20de3e42ae8a49154f"));
+        assert(genesis.hashMerkleRoot == uint256S("0x214037fe78c284b2ee501e0c68d629126d394a4fa04f23f88699af72ceedcccd"));
 
         // Note that of those with the service bits flag, most only support a subset of possible options
-        vSeeds.push_back(CDNSSeedData("loshan.co.uk", "seed-a.unifycoin.loshan.co.uk", true));
-        vSeeds.push_back(CDNSSeedData("thrasher.io", "dnsseed.thrasher.io", true));
-        vSeeds.push_back(CDNSSeedData("unifycointools.com", "dnsseed.unifycointools.com"));
-        vSeeds.push_back(CDNSSeedData("unifycoinpool.org", "dnsseed.unifycoinpool.org"));
-        vSeeds.push_back(CDNSSeedData("koin-project.com", "dnsseed.koin-project.com"));
-
-        base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,48);
+        vSeeds.push_back(CDNSSeedData("145.239.89.215", "145.239.89.215"), true);
+        vSeeds.push_back(CDNSSeedData("158.69.212.99", "158.69.212.99"), true);
+        vSeeds.push_back(CDNSSeedData("node1.unifycoin.ovh", "node1.unifycoin.ovh"));
+        vSeeds.push_back(CDNSSeedData("node2.unifycoin.ovh", "node2.unifycoin.ovh"));
+        vSeeds.push_back(CDNSSeedData("node3.unifycoin.ovh", "node3.unifycoin.ovh"));
+        
+        base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,68);
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,5);
         base58Prefixes[SCRIPT_ADDRESS2] = std::vector<unsigned char>(1,50);
-        base58Prefixes[SECRET_KEY] =     std::vector<unsigned char>(1,176);
+        base58Prefixes[SECRET_KEY] =     std::vector<unsigned char>(1,196);
         base58Prefixes[EXT_PUBLIC_KEY] = boost::assign::list_of(0x04)(0x88)(0xB2)(0x1E).convert_to_container<std::vector<unsigned char> >();
         base58Prefixes[EXT_SECRET_KEY] = boost::assign::list_of(0x04)(0x88)(0xAD)(0xE4).convert_to_container<std::vector<unsigned char> >();
 
@@ -142,30 +142,28 @@ public:
 
         checkpointData = (CCheckpointData) {
             boost::assign::map_list_of
-            (  1500, uint256S("0x841a2965955dd288cfa707a755d05a54e45f8bd476835ec9af4402a2b59a2967"))
-            (  4032, uint256S("0x9ce90e427198fc0ef05e5905ce3503725b80e26afd35a987965fd7e3d9cf0846"))
-            (  8064, uint256S("0xeb984353fc5190f210651f150c40b8a4bab9eeeff0b729fcb3987da694430d70"))
-            ( 16128, uint256S("0x602edf1859b7f9a6af809f1d9b0e6cb66fdc1d4d9dcd7a4bec03e12a1ccd153d"))
-            ( 23420, uint256S("0xd80fdf9ca81afd0bd2b2a90ac3a9fe547da58f2530ec874e978fce0b5101b507"))
-            ( 50000, uint256S("0x69dc37eb029b68f075a5012dcc0419c127672adb4f3a32882b2b3e71d07a20a6"))
-            ( 80000, uint256S("0x4fcb7c02f676a300503f49c764a89955a8f920b46a8cbecb4867182ecdb2e90a"))
-            (120000, uint256S("0xbd9d26924f05f6daa7f0155f32828ec89e8e29cee9e7121b026a7a3552ac6131"))
-            (161500, uint256S("0xdbe89880474f4bb4f75c227c77ba1cdc024991123b28b8418dbbf7798471ff43"))
-            (179620, uint256S("0x2ad9c65c990ac00426d18e446e0fd7be2ffa69e9a7dcb28358a50b2b78b9f709"))
-            (240000, uint256S("0x7140d1c4b4c2157ca217ee7636f24c9c73db39c4590c4e6eab2e3ea1555088aa"))
-            (383640, uint256S("0x2b6809f094a9215bafc65eb3f110a35127a34be94b7d0590a096c3f126c6f364"))
-            (409004, uint256S("0x487518d663d9f1fa08611d9395ad74d982b667fbdc0e77e9cf39b4f1355908a3"))
-            (456000, uint256S("0xbf34f71cc6366cd487930d06be22f897e34ca6a40501ac7d401be32456372004"))
-            (638902, uint256S("0x15238656e8ec63d28de29a8c75fcf3a5819afc953dcd9cc45cecc53baec74f38"))
-            (721000, uint256S("0x198a7b4de1df9478e2463bd99d75b714eab235a2e63e741641dc8a759a9840e5"))
+            (     0, uint256S("0x943dc625657143761f0f01dbf08c45a02260fc4089a27b20de3e42ae8a49154f"))
+            (   500, uint256S("0x95d87809cec718f83f3d981a838c6414a3e49bdc742aa836d72d33b8ff14ec5a"))
+            (  2000, uint256S("0x2928ac70a5d33bc1c560a45a23c4009be9bd869e9a17b2f4762bf9cf5130b946"))
+            (  4000, uint256S("0x5c9aa4ea2e6331efc3e41e1e596f246cc1f7e08a7a500cebed0e8741ba737b05"))
+            (  8000, uint256S("0xe229e92a13e7ac62c5e080ec6b4bedcc38c3ed6e704996c55fa11206739197cd"))
+            ( 16000, uint256S("0x540359eefa6bb3d535b74abdde7a5f67500fed3ffddb0ff807fb25a8acd33865"))
+            ( 24000, uint256S("0x98c5bb3b486104474cc313ca10be6816eb4ca69ea0b39b19216b66dfdb586626"))
+            ( 50000, uint256S("0x3f7347640e627d03615309db3f408e91a1b073bb81da2b3cf835ae10fddf8c09"))
+            ( 80000, uint256S("0x0b5d200a50e5d9e8c5b7318a84e253510666e4059ba45252b197d38638b0572d"))
+            (120000, uint256S("0x8f4f965a51f65caedeb5f493d1f33b6dd93bf37a89815a1a6e5c8edeec7b5ae2"))
+            (140000, uint256S("0x7c8a48a883f2c3124fd2cc65d337443ac278efc5b77a9347511be4e4a86d6f4d"))
+            (160000, uint256S("0xf483d29a7524ed37c7dbd1e82dc4847e1a5769e947d59b89f502cada2ee19e33"))
+            (180000, uint256S("0x6a5f61ce7503279dcea64300beeeb14598f8e97ca22dabadb3cb3b26f864fb37"))
+            (190000, uint256S("0x99736ae204698534a474557f239a515ced958c78c3dde5f5d1e2cb6c899d8d87"))
+            (194397, uint256S("0x5bfe9d8d3ab8f12f3d30e8301bd6e332b2f15cfc5c5c479653abb5d9c3a8ee8b"))
         };
 
         chainTxData = ChainTxData{
-            // Data as of block b44bc5ae41d1be67227ba9ad875d7268aa86c965b1d64b47c35be6e8d5c352f4 (height 1155626).
-            1487715936, // * UNIX timestamp of last known number of transactions
-            9243806,  // * total number of transactions between genesis and that timestamp
-                    //   (the tx=... number in the SetBestChain debug.log lines)
-            0.06     // * estimated number of transactions per second after that timestamp
+            1497449710, // * UNIX timestamp of last known number of transactions
+            22050,      // * total number of transactions between genesis and that timestamp
+                        //   (the tx=... number in the SetBestChain debug.log lines)
+            120.0       // * estimated number of transactions per second after that timestamp
         };
     }
 };
